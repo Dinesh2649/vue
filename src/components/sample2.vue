626898421cefea5c430da1ad5105627762482815
Dinesh2649 <dineshreddy.vankara@gmail.com>
2019-07-11 20:02:53 +0530

    text

--- src/components/sample2.vue
+++ src/components/sample2.vue
@@ -1,34 +1,32 @@
 <template>
-  <div>
-    <input type="text" v-on:input="changeTitle">
-    <h1>{{title}}</h1>
-    
-  </div>
+<div>
+  <input type="text" v-on:input="changeTitle">
+  <h1>{{title}}</h1>
+ </div>
 </template>
-
 <script>
-export default {
-  name: "sample2",
-  data(){
-  return{
-       title:"reddy"
-   }
-  },
-  methods:{
-   changeTitle: function(event){
-     this.title=event.target.value;
-   }
+export default{
+name: "sample2",
+   data() {
+    return {
+    title:"anudeep"
+ 
   }
+},
+methods:{
+changeTitle: function(ev){
+this.title= ev.target.value;
 }
-
-
+}
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1{
-  color:red;
-  background-color:black;
+h1 {
+  color:blue;
+  
+
 }
 
 </style>
